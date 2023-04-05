9c9b665a3c2c54bbd865d6ae589841e7abddcc7c
polzovatel <nursejitsadirov@gmail.com>
2023-04-05 14:17:35 +0600

    success method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import dao.impl.HospitalDaoImpl;
+import model.Hospital;
 
 public class Main {
     public static void main(String[] args) {
-
+        HospitalDaoImpl hospitalDao = new HospitalDaoImpl();
+        Hospital hospital = new Hospital();
 
     }
 }
