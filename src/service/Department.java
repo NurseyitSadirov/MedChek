b8088aeb32c793b93faf1acc08be848ea8c4ca9c
polzovatel <nursejitsadirov@gmail.com>
2023-04-06 11:51:36 +0600

    Success DepartmentImpl method

--- src/service/Department.java
+++ src/service/Department.java
@@ -1,4 +1,12 @@
 package service;
 
+import java.util.List;
+
 public interface Department {
+
+    String addDepartmentToHospital(Long id, model.Department department);
+    List<model.Department> getAllDepartmentByHospital(Long id);
+    model.Department findDepartmentByName(String name);
+    void deleteDepartmentById(Long id);
+    String updateDepartmentById(Long id, model.Department department);
 }
